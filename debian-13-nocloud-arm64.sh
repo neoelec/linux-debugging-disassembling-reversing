833d3f9848f03019c0fff6202403fe6a58009bb7
YOUNGJIN JOO <neoelec@gmail.com>
2026-03-07 19:44:13 +0900

    update qemu script
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- debian-13-nocloud-arm64.sh
+++ debian-13-nocloud-arm64.sh
@@ -7,6 +7,6 @@ qemu-system-aarch64 -m 2G -M virt,mte=on -cpu max \
     -device virtio-rng-device,rng=rng \
     -drive if=none,file=.qemu/$(basename $0 .sh).qcow2,id=hd0 \
     -device virtio-blk-device,drive=hd0 \
-    -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5522-:22,hostfwd=tcp::5531-:5531 \
+    -device e1000e,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5522-:22,hostfwd=tcp::5531-:2331 \
     -virtfs local,path=${HOME}/iHDD00,mount_tag=iHDD00,security_model=passthrough,id=iHDD00 \
     -nographic
--- debian-13-nocloud-riscv64.sh
+++ debian-13-nocloud-riscv64.sh
@@ -7,6 +7,6 @@ qemu-system-riscv64 -m 2G -M virt -cpu rv64 \
     -device virtio-rng-device,rng=rng \
     -drive if=none,file=.qemu/$(basename $0 .sh).qcow2,id=hd0 \
     -device virtio-blk-device,drive=hd0 \
-    -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::6622-:22,hostfwd=tcp::6631-:6631 \
+    -device e1000e,netdev=net0 -netdev user,id=net0,hostfwd=tcp::6622-:22,hostfwd=tcp::6631-:2331 \
     -virtfs local,path=${HOME}/iHDD00,mount_tag=iHDD00,security_model=passthrough,id=iHDD00 \
     -nographic
